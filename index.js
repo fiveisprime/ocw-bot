12bd439119c858edd0e0ecc3971d2488d72a66c7
Matt Hernandez <matt@modulus.io>
2013-11-05 09:54:21 -0500

    Updated the bot constructor to cleanup binding.
    Events are now bound to a prototype method on the Bot object correctly.
    This makes the use of `this` a lot cleaner and removes the weirdness of
    having `var _this = this;` in the constructor.
    Updated the index script to include some comments.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var irc = require('irc');
 
+//
+// Initialize the bot with a new IRC client instance.
+//
 require('./lib/bot')(new irc.Client('irc.freenode.net', 'lolbot', {
   channels: ['#outofcontrolware']
 }));
