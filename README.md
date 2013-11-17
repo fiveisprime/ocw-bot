53d67fbbebc75a83dc23428a349039965a80bf26
Matt Hernandez <matt@modulus.io>
2013-11-17 10:42:53 -0500

    Updated documentation.
    Added some code comments and a blurb about how to use the migrator
    script to the readme.

--- README.md
+++ README.md
@@ -5,6 +5,22 @@ ocw-bot
 
 IRC bot for lulz in the ocw channel.
 
+# Text data and LevelDB
+
+There is a plain text to level migrator that will take data from a text file,
+split it on newline characters, then dump that data to the database with the
+key as the index of that data in the array. This is used to add additional
+lols to the randomness.
+
+The migration command from the root directory is
+
+    $ node ./migrators/text-t-level.js <path/to/text/file.txt>
+
+If a file specifier is passed to the script, the data in the file will be added
+to the database; otherwise, the script will calculate the total number of lols
+in the level database and update its lolcount key which is used to grab data
+randomly.
+
 # Contributions
 
 Be sure that `make test` exits with 0 before pushing your changes. :)
--- migrators/text-to-level.js
+++ migrators/text-to-level.js
@@ -7,12 +7,20 @@ if (process.argv.length >= 3)
 else
   getObjectCount();
 
+//
+// Migrate plain text data to the database.
+//
+// This will overwrite any existing data in the database; it's possible to make
+//    it smart by checking the 'lolcount' before putting the data in there and
+//    starting at that index.
+//
 function migrateFileData(file) {
   fs.readFile(file, { encoding: 'utf8' }, function(err, data) {
     if (err) throw err;
 
     var bits = data.split('\n');
 
+    // This should be updated to start at the index of 'lolcount'.
     for (var i = 0; i < bits.length; i++) {
       if (bits[i].length === 0) continue;
       db.put(i, bits[i].trim(), function(err) {
@@ -22,6 +30,10 @@ function migrateFileData(file) {
   });
 }
 
+//
+// Gets the total count of objects in the database. This is useful for the
+//    `!text` command where we base our randomized number on this count.
+//
 function getObjectCount() {
   var values = [];
 
@@ -30,8 +42,11 @@ function getObjectCount() {
       values.push(data);
     })
     .on('close', function() {
+      // Log out the count in color.
       console.log('\n  \033[36mfound %d lols in the database\033[0m', values.length);
       console.log('  \033[33mupdated database lolcount\033[0m\n');
+
+      // Update/add the 'lolcount' in the database.
       db.put('lolcount', values.length);
     });
 }
