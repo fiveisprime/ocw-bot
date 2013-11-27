5465da12411c6e74aa7f6592b8441daba48fc425
Matt Hernandez <matt@modulus.io>
2013-11-26 21:12:35 -0500

    Updated texts to be a reasonable size...

--- migrators/text-to-json.js
+++ migrators/text-to-json.js
@@ -6,14 +6,17 @@ fs.readFile(process.argv[2], { encoding: 'utf8' }, function(err, data) {
   if (err) throw err;
 
   var bits = data.split('\n')
-    , out = [];
+    , out  = []
+    , i    = 0;
 
-  for (var i = 0; i < bits.length; i++) {
+  for (; i < bits.length; i++) {
+    // Keep the output at a reasonable size...
+    if (i === 700) break;
     if (bits[i].length === 0) continue;
     out.push(bits[i].trim());
   }
 
-  fs.writeFile(process.argv[3], JSON.stringify(out), function(err) {
+  fs.writeFile(process.argv[3], JSON.stringify(out, null, '  '), function(err) {
     if (err) throw err;
     console.log('\n  \033[36mSaved to %s\033[0m\n', process.argv[3]);
   });
