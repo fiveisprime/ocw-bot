24377f9e0884bf65bf81cd60a3175997b8833033
Matt Hernandez <matt@modulus.io>
2013-11-26 20:49:23 -0500

    Changed texts to JSON.
    Added a text to JSON migrator and changed the texts to a json store
    rather than a level db store.

---
+++ migrators/text-to-json.js
@@ -0,0 +1,21 @@
+var level = require('level')
+  , fs    = require('fs');
+
+if (process.argv.length < 4) process.exit(1);
+
+fs.readFile(process.argv[2], { encoding: 'utf8' }, function(err, data) {
+  if (err) throw err;
+
+  var bits = data.split('\n')
+    , out = [];
+
+  for (var i = 0; i < bits.length; i++) {
+    if (bits[i].length === 0) continue;
+    out.push(bits[i].trim());
+  }
+
+  fs.writeFile(process.argv[3], JSON.stringify(out), function(err) {
+    if (err) throw err;
+    console.log('\n  \033[36mSaved to %s\033[0m\n', process.argv[3]);
+  });
+});
